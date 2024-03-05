41371d8a6e02402f17a169971468ffa228a44956
0xshoegazer <0xshoegazer@gmail.com>
2024-03-05 10:46:41 -0500

    mode setup

--- src/utils/pricing.ts
+++ src/utils/pricing.ts
@@ -5,41 +5,23 @@ import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 import { exponentToBigDecimal, safeDiv } from '../utils/index'
 
 const WETH_ADDRESS = '0x4200000000000000000000000000000000000006'
-const WETH_USDBC_03_POOL = '0xef3c164b0fee8eb073513e88ecea280a58cc9945'
+const WETH_USDC_03_POOL = '' // TODO: Need this pool for pricing
 
 // token where amounts should contribute to tracked volume and liquidity
 // usually tokens that many tokens are paired with
 export let WHITELIST_TOKENS: string[] = [
   WETH_ADDRESS, // WETH
-  '0x2ae3f1ec7f1f5012cfeab0185bfc7aa3cf0dec22', // cbETH
-  '0x78a087d713be963bf307b18f2ff8122ef9a63ae9', // BSWAP
-  '0xd5046b976188eb40f6de40fb527f89c05b323385', // BSX
-  '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913', // Native USDC
-  '0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca', // USDbC
-  '0xeb466342c4d449bc9f53a865d5cb90586f405215', // axlUSDC
-  '0x50c5725949a6f0c72e6c4a641f24049a917db0cb', // DAI
-  '0x4a3a6dd60a34bb2aba60d73b4c88315e9ceb6a3d', // MIM
-  '0xb79dd08ea68a908a97220c76d19a6aa9cbde4376', // USD+
-  '0x65a2508c429a6078a7bc2f7df81ab575bd9d9275', // DAI+
-  '0x4788de271f50ea6f5d5d2a5072b8d3c61d650326', // BASIN
-  '0x0a074378461fb7ed3300ea638c6cc38246db4434', // EDE
-  '0xab8a1c03b8e4e1d21c8ddd6edf9e07f26e843492', // OGRE
-  '0x6b4712ae9797c199edd44f897ca09bc57628a1cf', // UNIDX
-  '0x8901cb2e82cc95c01e42206f8d1f417fe53e7af0', // YFX
-  '0xa3d1a8deb97b111454b294e2324efad13a9d8396', // OVN
-  '0xbf1aeA8670D2528E08334083616dD9C5F3B087aE', // MAI
-  '0x4A0a76645941d8C7ba059940B3446228F0DB8972' // CAROL
+  '0xFDa619b6d20975be80A10332cD39b9a4b0FAa8BB', // SMD
+  '0xd988097fb8612cc24eeC14542bC03424c656005f', // Native USDC
+  '0xE7798f023fC62146e8Aa1b36Da45fb70855a77Ea', // DAI
+  '0xf0F161fDA2712DB8b566946122a5af183995e2eD', // USDT
+  '0xcDd475325D6F564d27247D1DddBb0DAc6fA0a5CF' // WBTC
 ]
 
 let STABLE_COINS: string[] = [
-  '0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca', // USDbC
-  '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913', // Native USDC
-  '0xeb466342c4d449bc9f53a865d5cb90586f405215', // axlUSDC
-  '0x50c5725949a6f0c72e6c4a641f24049a917db0cb', // DAI
-  '0x4a3a6dd60a34bb2aba60d73b4c88315e9ceb6a3d', // MIM
-  '0xb79dd08ea68a908a97220c76d19a6aa9cbde4376', // USD+
-  '0x65a2508c429a6078a7bc2f7df81ab575bd9d9275', // DAI+
-  '0xbf1aeA8670D2528E08334083616dD9C5F3B087aE' // MAI
+  '0xd988097fb8612cc24eeC14542bC03424c656005f', // Native USDC
+  '0xE7798f023fC62146e8Aa1b36Da45fb70855a77Ea', // DAI
+  '0xf0F161fDA2712DB8b566946122a5af183995e2eD' // USDT
 ]
 
 let MINIMUM_ETH_LOCKED = BigDecimal.fromString('2')
@@ -58,13 +40,15 @@ export function sqrtPriceX96ToTokenPrices(sqrtPriceX96: BigInt, token0: Token, t
 }
 
 export function getEthPriceInUSD(): BigDecimal {
-  // fetch eth price for a stablecoin
-  let usdcPool = Pool.load(WETH_USDBC_03_POOL) // USDbC is token1
-  if (usdcPool !== null) {
-    return usdcPool.token1Price
-  } else {
-    return ZERO_BD
-  }
+  // // fetch eth price for a stablecoin
+  // let usdcPool = Pool.load(WETH_USDC_03_POOL) // USDC is token1
+  // if (usdcPool !== null) {
+  //   return usdcPool.token1Price
+  // } else {
+  //   return ZERO_BD
+  // }
+
+  return ZERO_BD
 }
 
 /**
